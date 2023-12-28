ae1a6a56a349d820f94e65aaf4e81b9be1c1ef50
Eugenia <mbp@MacBookPro.Dlink>
2023-12-28 20:04:50 +0300

    edit config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const config = {
-  entry: './src/index.js',
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  // entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    // path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     open: true,
@@ -25,15 +27,19 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
     ],
   },
 };
 
-module.exports = () => {
+/* module.exports = () => {
   if (isProduction) {
     config.mode = 'production';
   } else {
     config.mode = 'development';
   }
   return config;
-};
+}; */
