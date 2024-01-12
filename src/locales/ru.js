49d6109864c2f8ca97951fe1f350d6a9321593f5
Eugenia <mbp@MacBookPro.local>
2024-01-12 20:12:12 +0300

    edit and add index.js

---
+++ src/locales/ru.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    headers: {
+      h1: 'RSS агрегатор',
+      p: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    },
+    form: {
+      input: 'Ссылка RSS',
+      submit: 'Добавить',
+    },
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    feedback: {
+      success: 'RSS успешно загружен',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRss: 'Ресурс не содержит валидный RSS',
+      duplicate: 'RSS уже существует',
+    },
+  },
+};
