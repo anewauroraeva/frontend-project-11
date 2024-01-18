b1661320fc03d0497fd0d6e486c9a73961a5fd37
Eugenia <karandashevaa@gmail.com>
2024-01-18 18:12:46 +0300

    edit

--- src/controller.js
+++ src/controller.js
@@ -1,76 +1,92 @@
 import onChange from 'on-change';
 import i18n from 'i18next';
-import validateURL from './validate.js';
+import * as yup from 'yup';
+// import _ from 'lodash';
 import render from './view.js';
 import ru from './locales/index.js';
-import yup from 'yup';
+
+const validateURL = async (url, addedLinks, i18nInstance) => {
+  yup.setLocale({
+    mixed: {
+      notOneOf: i18nInstance.t('feedback.duplicate'),
+    },
+    string: {
+      url: i18nInstance.t('feedback.invalidUrl'),
+    },
+  });
+
+  const schema = yup.string()
+    .trim()
+    .required()
+    .url(i18nInstance.t('feedback.invalidUrl'))
+    .notOneOf(addedLinks, i18nInstance.t('feedback.duplicate'));
+  try {
+    return schema.validate(url);
+  } catch (error) {
+    return error.message;
+  }
+};
 
 const app = async () => {
-  const state = {
-    isValid: null,
-    links: [],
-    defaultLanguage: 'ru',
-    submitState: 'idle',
-    submitError: '',
-    feedback: {
-      success: i18n.t('feedback.example'),
-      invalidRss: i18n.t('example'),
-      invalidUrl: '',
-      duplicate: '',
-    },
-  };
-
-  console.log(state.feedback.invalidRss);
-
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
     submit: document.querySelector('button[type=submit]'),
     feedback: document.querySelector('.feedback'),
   };
+  const { form } = elements; // , input, submit
 
-  const { defaultLanguage } = state.defaultLanguage;
   const i18nInstance = i18n.createInstance();
   await i18nInstance.init({
-    defaultLanguage,
+    lng: 'ru',
     debug: false,
     resources: { ru },
-  })
-    .then(() => {
-      yup.setLocale({
-        mixed: {
-          notOneOf: i18nInstance.t('links'),
-        },
-        string: {
-          url: i18nInstance.t('feedback.invalidUrl'),
-        },
-      }),
-    });
-
-  const watchedState = onChange(state, render(watchedState, i18nInstance, elements));
-
-  elements.form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const result = await validateURL(elements.input.value, state.links);
-    // rename result
-    // console.log(result); +
-    if (!result) {
-      watchedState.isValid = true;
-      const formData = new FormData(elements.form, elements.submit);
-      const url = formData.get('url');
-      // console.log(url); +
-      watchedState.links.push(url);
-    }
-    if (result) {
-      watchedState.isValid = false;
-      watchedState.submitError = result;
-    }
-    console.log(state);
-
-    // render(state, elements);
   });
 
-  // render(watchedState, i18nInstance, elements);
+  const state = {
+    form: {
+      isValid: null,
+      formState: 'idle', // 'sending' 'sent' 'failed'
+    },
+    addedLinks: [],
+    errorMessage: '',
+    feedback: {
+      success: i18nInstance.t('feedback.success'),
+      invalidRss: i18nInstance.t('feedback.invalidRss'),
+      invalidUrl: i18nInstance.t('feedback.invalidUrl'),
+      duplicate: i18nInstance.t('feedback.duplicate'),
+      networkError: i18nInstance.t('feedback.networkError'),
+    },
+  };
+  console.log(state); // +
+
+  const watchedState = onChange(state, () => {
+    render(watchedState, elements);
+  });
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    watchedState.formState = 'sending';
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    validateURL(url, watchedState.addedLinks, i18nInstance)
+      .then((data) => {
+        console.log('valid');
+        watchedState.form.isValid = true;
+        watchedState.errorMessage = '';
+        // const validUrl = { id: _.uniqueId(), url: data };
+        watchedState.addedLinks.push(data);
+      })
+      .catch((err) => {
+        console.log('invalid');
+        watchedState.form.isValid = false;
+        console.log(err.message);
+        watchedState.errorMessage = err.message;
+      });
+    watchedState.form.isValid = null;
+    console.log(watchedState);
+  });
+  console.log(watchedState);
 };
 
-export default app; // or app???
+export default app;
--- src/view.js
+++ src/view.js
@@ -1,31 +1,22 @@
 // import i18next from 'i18next';
 
-const render = (state, instance, elements) => {
-  const { form, feedback } = elements;
-  if (state.isValid) {
+const render = (watchedState, elements) => {
+  const { form, input, feedback } = elements;
+  if (watchedState.form.isValid) {
+    input.classList.remove('is-invalid');
     feedback.classList.remove('text-danger');
     feedback.classList.add('text-success');
-    feedback.textContent = state.successMessage;
+    feedback.textContent = watchedState.feedback.success;
     form.reset();
     form.focus();
   }
-  if (state.isValid === false) {
+  if (watchedState.form.isValid === false) {
+    input.classList.add('is-invalid');
     feedback.classList.remove('text-success');
     feedback.classList.add('text-danger');
-    feedback.textContent = state.submitError;
+    console.log(watchedState.errorMessage);
+    feedback.textContent = watchedState.errorMessage;
   }
-  /* const watchedState = onChange(state, () => {
-    const { form, feedback } = elements;
-    // const checkDiv = document.createElement('div');
-    feedback.textContent = 'check div';
-    if (watchedState.isValid) {
-      feedback.classList.remove('text-danger');
-      feedback.classList.add('text-success');
-      feedback.textContent = watchedState.submitMessage;
-      form.reset();
-      form.focus();
-    }
-  }); */
 };
 
 export default render;
