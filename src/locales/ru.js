4a8df16855924e20f90e788b4f07244996846f6d
Eugenia <karandashevaa@gmail.com>
2024-01-18 18:11:01 +0300

    add network error

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -14,9 +14,10 @@ export default {
     example: 'Пример: https://ru.hexlet.io/lessons.rss',
     feedback: {
       success: 'RSS успешно загружен',
-      fb_invalidUrl: 'Ссылка должна быть валидным URL',
-      fb_invalidRss: 'Ресурс не содержит валидный RSS',
-      fb_duplicate: 'RSS уже существует',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRss: 'Ресурс не содержит валидный RSS',
+      duplicate: 'RSS уже существует',
+      networkError: 'Ошибка сети',
     },
   },
 };
